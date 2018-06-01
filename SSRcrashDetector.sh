9ab80516d3783590965f6bbe785b998d62e66e91
AceBee007 <zykgcz@gmail.com>
2018-06-01 16:45:46 +0900

    Create SSRcrashDetector.sh

---
+++ SSRcrashDetector.sh
@@ -0,0 +1,19 @@
+while true
+do
+    # Get dead process information
+    deadProcess="$(ps -ax | grep "python" | grep "D" | grep -v "grep")"
+    # Get PID of the dead process
+    for str in $deadProcess; do
+        deadPID=$str
+        break
+    done
+    # If $deadPID is not NULL (there is a number in deadPID)
+    if [ -n "$deadPID" ] ; then
+        # The ssr-server is dead, restart a ssr-serve
+        eval "sudo reboot -f"
+    else
+        # Do nothing
+        :
+    fi
+    sleep 20
+done

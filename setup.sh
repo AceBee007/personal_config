6c328c0814b47a1d1c1f0575480d553a93b466f8
AceBee007 <zykgcz@gmail.com>
2019-06-04 15:19:04 +0900

    Add bash_aliases

--- setup.sh
+++ setup.sh
@@ -4,6 +4,10 @@ sudo apt autoremove
 
 sudo -E apt install vim tmux -y
 
+cd ~
+
 wget https://raw.githubusercontent.com/AceBee007/my_vimrc_file/master/.tmux.conf
 
 wget https://raw.githubusercontent.com/AceBee007/my_vimrc_file/master/.vimrc
+
+wget https://raw.githubusercontent.com/AceBee007/my_vimrc_file/master/.bash_aliases

a9717c6dcd6060b962301b2de38611a96e4620a5
AceBee007 <zykgcz@gmail.com>
2019-06-04 15:14:18 +0900

    Create setup.sh
    基本のセットアップスクリプトを作った

---
+++ setup.sh
@@ -0,0 +1,9 @@
+sudo -E apt update
+sudo -E apt upgrade -y
+sudo apt autoremove
+
+sudo -E apt install vim tmux -y
+
+wget https://raw.githubusercontent.com/AceBee007/my_vimrc_file/master/.tmux.conf
+
+wget https://raw.githubusercontent.com/AceBee007/my_vimrc_file/master/.vimrc

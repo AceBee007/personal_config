539ac4235b1f1c92d7255d067f7fd8e4a21e096c
AceBee007 <zykgcz@gmail.com>
2020-11-15 10:43:59 +0900

    removed git clone vundle

--- setup.sh
+++ setup.sh
@@ -6,8 +6,6 @@ sudo -E apt install vim tmux nethogs htop wget git -y
 
 cd ~
 
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
 curl -skL https://raw.githubusercontent.com/AceBee007/my_vimrc_file/master/.tmux.conf > ~/.tmux.conf
 
 curl -skL https://raw.githubusercontent.com/AceBee007/my_vimrc_file/master/.vimrc > ~/.vimrc

ca17f05a20100344fae96781b038f4a9dcf93bb6
AceBee007 <zykgcz@gmail.com>
2021-01-04 15:17:49 +0900

    Create install_docker.sh

---
+++ install_docker.sh
@@ -0,0 +1,13 @@
+sudo -E apt-get update && sudo -E apt-get upgrade -y && sudo -E apt-get autoremove -y
+# make Downloads directory
+mkdir ~/Downloads
+# download install script
+curl -fsSL https://get.docker.com -o ~/Downloads/get-docker.sh
+# install docker
+sudo -E sh ~/Downloads/get-docker.sh
+# install docker-compose
+sudo -E curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# change the permission
+sudo chmod +x /usr/local/bin/docker-compose
+# check the version
+docker-compose --version

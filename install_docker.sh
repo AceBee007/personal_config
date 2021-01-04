e0f407ae1eef1b25efd7f99dac6d9b12a97658d8
AceBee007 <zykgcz@gmail.com>
2021-01-04 16:46:27 +0900

    delete docker-compose install command
    need a new script to fix aarcch64 architecture (for raspi)

--- install_docker.sh
+++ install_docker.sh
@@ -5,7 +5,3 @@ mkdir ~/Downloads
 curl -fsSL https://get.docker.com -o ~/Downloads/get-docker.sh
 # install docker
 sudo -E sh ~/Downloads/get-docker.sh
-# install docker-compose
-sudo -E curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-# change the permission
-sudo chmod +x /usr/local/bin/docker-compose

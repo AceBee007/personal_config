e822b3db76004b92c208c26c92cacfd8ac26c579
AceBee007 <zykgcz@gmail.com>
2021-05-08 16:43:52 +0900

    add docker to sudogroup

--- install_docker.sh
+++ install_docker.sh
@@ -5,3 +5,9 @@ mkdir ~/Downloads
 curl -fsSL https://get.docker.com -o ~/Downloads/get-docker.sh
 # install docker
 sudo -E sh ~/Downloads/get-docker.sh
+# add docker to sudo group
+sudo groupadd docker
+# add this user ($USER) to docker group
+sudo usermod -aG docker $USER
+# Please re-login to read the config
+echo Please re-login to apply the new configuration

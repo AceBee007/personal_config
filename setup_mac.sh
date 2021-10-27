05a733f1f330962e91d747871acd8bda037d9b59
AceBee007 <zykgcz@gmail.com>
2021-10-27 16:55:33 +0900

    Create setup_mac.sh

---
+++ setup_mac.sh
@@ -0,0 +1,16 @@
+# install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+# install favorate apps
+brew update
+brew install bat
+brew install lazydocker
+brew install tldr
+brew install glances
+brew install htop
+brew install httpie
+
+# install homebrew-caks and install iterm2
+brew install cask
+brew cask install iterm2
+
